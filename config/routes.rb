4c6aaad8c72c9fbe22002773a65dbd27bc18013b
Fprince333 <lwilsonsmith@gmail.com>
2018-02-02 13:15:35 -0500

    Add redirect functionality to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/:id", to: redirect("https://www.archsystems.com/%{id}.aspx")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
